a1cb38b74c6caa6a8d5ce4f653de0208618d1b3b
Florian Nücke <florian@nuecke.de>
2012-05-17 23:14:31 +0200

    trying nicer textures

---
+++ gold.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   gold.h
+ * Author: fnuecke
+ *
+ * Created on May 17, 2012, 9:01 PM
+ */
+
+#ifndef GOLD_H
+#define	GOLD_H
+
+#include "vmath.h"
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+    void MP_AddGoldPile(const vec2* position, unsigned int amount);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* GOLD_H */
+
